d8a4391f67ca3b5e54e430a2eb32dd452941bc48
Valentin Immler-Valchev <vvvalvalchev@gmai.com>
2021-05-15 17:33:25 +0200

    Rendering fixed

--- eventTracker/src/Controller/EventController.php
+++ eventTracker/src/Controller/EventController.php
@@ -9,39 +9,56 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EventController extends AbstractController {
-    // Sepicy the Route
+    // Specify the Route
 /**
- * @Route("/")
+ * @Route("/", name="event_list")
  * @Method({"GET"})
  */
 
     public function index(): Response{
         // return new Response('<html><body>Hello</body></html>');
     
-    $events = ['Event 1', 'Event 2'];
+    $events = $this->getDoctrine()->getRepository
+    (Event::class)->findAll();
 
         return $this->render('events/index.html.twig', array
     ('events'=> $events));
     }
-
 /**
- * @Route("/event/save")
- */
+* @Route("/event/{id}", name="event_show")
+* @Method({"GET"})
+*/
+public function show($id){
+    $event=$this->getDoctrine()->getRepository
+    (Event::class)->find($id);
 
-   public function save(){
-       $entityManager=$this->getDoctrine()->getManager();
+    return $this->render('events/show.html.twig', 
+    array('event'=>$event));
+}
+    
 
-       $event = new Event();
-       $event->setTitle('Event One');
-       $event->setDescription('This is the description for Event One');
-       
-       //What persist does is that it tells us we would like to eventually save 
-       $entityManager->persist($event);
-
-       $entityManager->flush();
-
-       return new Response('Saves an Event with the id of
-       '.$event->getId());
-   } 
 
 }
+
+
+
+
+// /**
+//  * @Route("/event/save")
+//  */
+
+//    public function save(){
+//        $entityManager=$this->getDoctrine()->getManager();
+
+//        $event = new Event();
+//        $event->setTitle('Event One');
+//        $event->setDescription('This is the description for Event One');
+       
+//        //What persist does is that it tells us we would like to eventually save 
+//        $entityManager->persist($event);
+
+//        $entityManager->flush();
+
+//        return new Response('Saves an Event with the id of
+//        '.$event->getId());
+//    } 
